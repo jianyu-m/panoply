80b571e2023512cd7be0998d68ded49115a65972
max <jianyu@hku.hk>
2017-12-25 21:25:49 +0800

    init

---
+++ src/TopensslEnclave/TopensslEnclave.cpp
@@ -0,0 +1,5 @@
+#include "TopensslEnclave.h"
+#include <sys/stat.h>
+int call_me() {
+  mkdir("/home/jianyu/aaaaaa4", (mode_t)0);
+}
---
+++ src/TopensslEnclave/TopensslEnclave.h
@@ -0,0 +1,13 @@
+#ifndef _ENCLAVE_H_
+#define _ENCLAVE_H_
+
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+int call_me();
+#if defined(__cplusplus)
+}
+#endif
+
+#endif /* !_ENCLAVE_H_ */
